64a9c0d7e6a305b3dde917c07e7ce4ef11b2a6ae
WGL <wanggilim@gmail.com>
2018-10-19 14:02:35 +0900

    회원가입-약식 완료 (회원가입시 아이디 쿠키 저장하여 index.html 로 넘겨짐.)

--- smartbogo/WebContent/intro/js/index.js
+++ smartbogo/WebContent/intro/js/index.js
@@ -3,6 +3,15 @@
  */
 
 $(function() {
+	var loginbar = $('#loginbar');
+	var id = $.cookie("id");
+		
+	if (id == undefined) {
+		loginbar.html('<li><a href="./signup/signup.html"><span class="glyphicon glyphicon-user"></span> 회원가입</a></li><li><a href="#" id="login_p"><span class="glyphicon glyphicon-log-in"></span> 로그인</a></li>');
+	} else {
+		loginbar.html('<li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li><li style="height:34px;margin-top:16px;">' + id + '님 환영합니다!</li>');
+	}
+	
 	$("#intro_carousel").carousel({
 		interval: 1500
 	});
--- smartbogo/WebContent/signup/js/signup.js
+++ smartbogo/WebContent/signup/js/signup.js
@@ -3,5 +3,97 @@
  */
 
 $(function() {
+	var cbUseAgree = $("#use_agree_cb");
+	var cbInfoAgree = $("#user_info_cb");
+	var lblUseAgree = $("label[for='use_agree']");
+	var lblInfoAgree = $("label[for='user_info']");
+	
+	$("#agree").attr({
+		"class": "btn btn-default active"
+	});
+	
+	lblUseAgree.click(function(){
+		if (cbUseAgree.prop("checked") == false) {
+			cbUseAgree.prop("checked", true);
+		} else {
+			cbUseAgree.prop("checked", false);
+		}
+		
+		if (cbUseAgree.is(":checked") == true && cbInfoAgree.is(":checked") == true) {
+			$("#agree").attr({
+				"class": "btn btn-primary"
+			});
+		} else {
+			$("#agree").attr({
+				"class": "btn btn-default active"
+			});
+		}
+	});
+	
+	lblInfoAgree.click(function(){
+		if (cbInfoAgree.prop("checked") == false) {
+			cbInfoAgree.prop("checked", true);
+		} else {
+			cbInfoAgree.prop("checked", false);
+		}
+		
+		if (cbUseAgree.is(":checked") == true && cbInfoAgree.is(":checked") == true) {
+			$("#agree").attr({
+				"class": "btn btn-primary"
+			});
+		} else {
+			$("#agree").attr({
+				"class": "btn btn-default active"
+			});
+		}
+	});
+	
+	cbUseAgree.click(function(){
+		if (cbUseAgree.is(":checked") == true && cbInfoAgree.is(":checked") == true) {
+			$("#agree").attr({
+				"class": "btn btn-primary"
+			});
+		} else {
+			$("#agree").attr({
+				"class": "btn btn-default active"
+			});
+		}
+	});
+	
+	cbInfoAgree.click(function(){
+		if (cbUseAgree.is(":checked") == true && cbInfoAgree.is(":checked") == true) {
+			$("#agree").attr({
+				"class": "btn btn-primary"
+			});
+		} else {
+			$("#agree").attr({
+				"class": "btn btn-default active"
+			});
+		}
+	});
+	
+	
+	$("#disagree").click(function(){
+		// 비동의
+		
+	});
+	
+	$("#agree").click(function(){
+		// 동의
+		// "smartbogo_test" 는 회원가입시 입력받을 아이디
+		
+//		$.cookie("id", "smartbogo_test");
+		// localhost 테스트 (1)
+		
+		$.cookie("id", "smartbogo_test", {
+			path: "/"
+		}); // localhost 테스트 (2)
+		
+//		$.cookie("id", "smartbogo_test", {
+//			path: "/", domain: "wanggilim.github.io/smartbogo"
+//		}); // Github Pages 테스트
+		
+		alert('가입을 환영합니다');
+	});
 	
 });
